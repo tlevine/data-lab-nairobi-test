a8ffb0a4fc01acb99829b27ccc515f34f652da04
Thomas Levine <_@thomaslevine.com>
2015-03-13 16:03:51 +0000

    -

--- question1.py
+++ question1.py
@@ -1,21 +1,12 @@
 '''
-A. You are requested to extract data from the
-`UN Iraq website <http://www.uniraq.org/index.php?option=com_k2&view=item&id=3344:un-casualty-figures-for-february-2015&Itemid=633&lang=en>_`.
-Please create a table in a 
-google spreadsheet that contains the casualty figures from November 2012 to 
-February 2015. Your extraction should be done programmatically (coded) to respond 
-to updates of the website (ie, it should be robust enough to react to changes of the 
-past estimates and to incorporate new estimates (March 2015). 
-
-B. Your resulting dataset should also contain the following metadata fields: source, 
-methodology, date of dataset, location, caveats and comments.
-
 I followed this guide.
 http://thomaslevine.com/!/web-sites-to-data-tables-in-depth/
 '''
 import datetime, argparse
+from collections import OrderedDict
 
-import requests, lxml.html, gdata
+import requests, lxml.html
+import googlesheets
 
 url = 'http://www.uniraq.org/index.php?option=com_k2&view=item&id=3344:un-casualty-figures-for-february-2015&Itemid=633&lang=en'
 
@@ -23,28 +14,57 @@ def download():
     response = requests.get(url)
     html = lxml.html.fromstring(response.text)
     rows = html.xpath('//table/tbody/tr')
-    head, *body = [[td.text_content() for td in row.xpath('td')] for row in rows]
+    data = [[td.text_content() for td in row.xpath('td')] for row in rows]
+    head = data.pop(0)
     if head != ['Month', 'Killed', 'Injured']:
         msg = 'The website has changed; you should update the %s scraper script.'
         raise AssertionError(msg % __file__)
-    return body
 
-def format_table(body, header = ['Date', 'Killed', 'Injured']):
-    yield header
-    for month, killed, injured in body:
+    metadata = OrderedDict([
+        ('source', url),
+        ('date of dataset', html.xpath('//span[@class="itemDateCreated"]/text()')[0].strip()),
+        ('location', html.xpath('//div[@class="itemIntroText"]/p/text()')[0].partition(',')[0]),
+        ('caveats', '\n'.join(html.xpath('//p[strong[contains(text(), "CAVEATS")]]/text()'))),
+        ('comments', '')])
+    return metadata, data
+
+
+def upload(metadata, data, header = ['Date', 'Killed', 'Injured']):
+    s = googlesheets.Spreadsheet.create('Casualty Figures')
+
+    # Delete everything.
+    while len(s.sheets) > 1:
+        s.sheets[-1].delete()
+    s.sheets[0].remove()
+    
+    data_sheet = s.sheets[0]
+    data_sheet.title = 'Data'
+    for month, killed, injured in data:
         date = datetime.datetime.strptime('1 %s' % month, '%d %B %Y').strftime('%Y/%m')
-        yield date, killed, injured
+        data_sheet.insert(OrderedDict(zip(header, (month, killed, injured))))
 
-desc = '''
-Extract data from the UN Iraq website
-http://www.uniraq.org/index.php?option=com_k2&view=item&id=3344:un-casualty-figures-for-february-2015&Itemid=633&lang=en
+    metadata_sheet = s.create_sheet('Metadata')
+    metadata_sheet.title = 'Metadata'
+    metadata_sheet.insert(metadata)
 
-Create a table in a google spreadsheet that contains the casualty figures from November 2012 on.
-The resulting dataset will also contain the following metadata fields: source, 
-methodology, date of dataset, location, caveats and comments.
+    return 'https://docs.google.com/spreadsheets/d/' + s.id
+
+var_note = '''
+You must set the "GOOGLE_USER" and "GOOGLE_PASSWORD" environment variables.
+For example,
+
+$ export GOOGLE_USER = thomas.levine@gmail.com
+$ export GOOGLE_PASSWORD = 0h23.uhrlcoe09u23b,rec,.e8hu23gde,..rhuoaruh2,rh.
+
+You must also turn on support for less-secure apps.
+https://www.google.com/settings/security/lesssecureapps
 '''
-p = argparse.ArgumentParser(description = desc)
-p.add_argument(
-
 def main():
-    list(format_table(download()))
+    import os, sys
+    if not {'GOOGLE_USER', 'GOOGLE_PASSWORD'}.issubset(os.environ.keys()):
+        sys.stderr.write(var_note)
+        sys.exit(1)
+    sys.stdout.write('Result is at %s.\n' % upload(*download()))
+
+if __name__ == '__main__':
+    main()
